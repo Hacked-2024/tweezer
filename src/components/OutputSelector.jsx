d295478ecc0128b082624b9230845b5e844d7a3e
Jacob Feng <peanutchowder@gmail.com>
2024-01-07 00:14:54 -0700

    Added checkbox style

--- src/components/OutputSelector.jsx
+++ src/components/OutputSelector.jsx
@@ -1,7 +1,14 @@
-const OutputSelector = () => {
+import './OutputSelector.css'
+
+const OutputSelector = ({ text }) => {
     return (
         <>
-            
+            <div className="checkbox-wrapper-7">
+                <input class="tgl tgl-ios" id="cb2-7" type="checkbox"/>
+                <label class="tgl-btn" for="cb2-7"></label>
+            </div>
         </>
     )
 }
+
+export default OutputSelector
