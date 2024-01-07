c28543d0c90e0d4cac477ee54eda9e1ed54e1b2e
Jacob Feng <peanutchowder@gmail.com>
2024-01-07 08:32:46 -0700

    Adjust widths and added new container

--- src/App.jsx
+++ src/App.jsx
@@ -15,8 +15,10 @@ const App = () => {
     <div className="parent-container">
       <h1>Tweezer</h1>
       <h3>API for detecting hate speech, misinformation, and harmful content</h3>
-      <ImageUploadForm currentlyChecked={currentlyChecked} setFormattedJsonString={setFormattedJsonString} setFormattedImageJsonString={setFormattedImageJsonString}/>
-      <TextEvaluationCheckboxes currentlyChecked={currentlyChecked} setCurrentlyChecked={setCurrentlyChecked}/>
+      <div className="upload-config-container">
+        <TextEvaluationCheckboxes currentlyChecked={currentlyChecked} setCurrentlyChecked={setCurrentlyChecked}/> 
+        <ImageUploadForm currentlyChecked={currentlyChecked} setFormattedJsonString={setFormattedJsonString} setFormattedImageJsonString={setFormattedImageJsonString}/>
+      </div>  
       <APIOutput formattedJsonString={formattedJsonString} formattedImageJsonString={formattedImageJsonString}/>
     </div>
   )
