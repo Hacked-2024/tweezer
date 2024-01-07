d2c87741c485e57f803af75f9d92ae55de17f47f
Jacob Feng <peanutchowder@gmail.com>
2024-01-07 00:19:32 -0700

    Added variable text

--- src/components/OutputSelector.jsx
+++ src/components/OutputSelector.jsx
@@ -3,9 +3,14 @@ import './OutputSelector.css'
 const OutputSelector = ({ text }) => {
     return (
         <>
-            <div className="checkbox-wrapper-7">
-                <input class="tgl tgl-ios" id="cb2-7" type="checkbox"/>
-                <label class="tgl-btn" for="cb2-7"></label>
+            <div className='output-selector-container'>
+                <div className="checkbox-wrapper-7">
+                    <input class="tgl tgl-ios" id="cb2-7" type="checkbox"/>
+                    <label class="tgl-btn" for="cb2-7"></label>
+                </div>
+                <div>
+                    {text}
+                </div>
             </div>
         </>
     )
