f874b3d49101e337097b9a71470fe41c55c6e3dd
Jacob Feng <peanutchowder@gmail.com>
2024-01-07 01:08:47 -0700

    Removed redundant div

--- src/components/OutputSelector.jsx
+++ src/components/OutputSelector.jsx
@@ -3,12 +3,10 @@ import './OutputSelector.css'
 const OutputSelector = ({ text, id }) => {
     return (
         <>
-            <div>
-                <div className="box">
-                    <input id={id} type="checkbox"/>
-                    <span className="check"></span>
-                    <label htmlFor={id}>{text}</label>
-                </div>
+            <div className="box">
+                <input id={id} type="checkbox"/>
+                <span className="check"></span>
+                <label htmlFor={id}>{text}</label>
             </div>
         </>
     )
