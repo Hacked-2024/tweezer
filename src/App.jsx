09c571aacff69062f2ba106515f671db5c1fecaa
Ashley Anderson <ashley.a1735@gmail.com>
2024-01-06 23:40:48 -0700

    json stuff

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import { useState } from "react";
 const App = () => {
   const [outputText, setOutputText] = useState("");
   return (
-    <div>
+    <div className="parent-container">
       <h1>Tweezer</h1>
       <h3>API for detecting hate speech, misinformation, and harmful content</h3>
       <ImageUploadForm setOutputText={setOutputText}/>
