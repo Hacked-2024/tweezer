b068c95695221ec28a6dbe7835ec92351db0eb55
Jacob Feng <peanutchowder@gmail.com>
2024-01-07 05:04:50 -0700

    Removed minors checkbox

--- src/components/TextEvaluationCheckboxes.jsx
+++ src/components/TextEvaluationCheckboxes.jsx
@@ -13,10 +13,9 @@ const TextEvaluationCheckboxes = ({ currentlyChecked, setCurrentlyChecked}) => {
                 <OutputSelector setCurrentlyChecked={setCurrentlyChecked} currentlyChecked={currentlyChecked} text={"Sexual Content"} id={"4"}/>
             </div>
             <div className="evaluation-checkbox-row">
-                <OutputSelector setCurrentlyChecked={setCurrentlyChecked} currentlyChecked={currentlyChecked} text={"Minors"} id={"5"}/>
-                <OutputSelector setCurrentlyChecked={setCurrentlyChecked} currentlyChecked={currentlyChecked} text={"Violence"} id={"6"}/>
-                <OutputSelector setCurrentlyChecked={setCurrentlyChecked} currentlyChecked={currentlyChecked} text={"Misinformation"} id={"7"}/>
-                <OutputSelector setCurrentlyChecked={setCurrentlyChecked} currentlyChecked={currentlyChecked} text={"Offensiveness"} id={"8"}/>
+                <OutputSelector setCurrentlyChecked={setCurrentlyChecked} currentlyChecked={currentlyChecked} text={"Violence"} id={"5"}/>
+                <OutputSelector setCurrentlyChecked={setCurrentlyChecked} currentlyChecked={currentlyChecked} text={"Misinformation"} id={"6"}/>
+                <OutputSelector setCurrentlyChecked={setCurrentlyChecked} currentlyChecked={currentlyChecked} text={"Offensiveness"} id={"7"}/>
             </div>
             
         </div>
